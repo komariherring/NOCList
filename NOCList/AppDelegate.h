e5b8c6d5cdea199357308a5febde2b320a636443
Komari Herring <komherr@gmail.com>
2016-08-29 23:50:09 -0400

    Initial Commit

---
+++ NOCList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NOCList
+//
+//  Created by Komari Herring on 8/29/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NOCList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NOCList
+//
+//  Created by Komari Herring on 8/29/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
